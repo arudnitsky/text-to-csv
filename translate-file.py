5859aedf223efdb8791d96c72f7ceca36430d931
Andy Rudnitsky <andy.rudnitsky@gmail.com>
2024-04-12 12:19:27 -0400

    Update to use new API return values

--- translate-file.py
+++ translate-file.py
@@ -16,9 +16,16 @@ class DeeplTranslateRequest(BaseModel):
 class DeeplTranslateResponse(BaseModel):
     text: str
     stressed_text: str
+    text_lemma: str
+    text_stressed_lemma: str
     translation: str
     stressed_translation: str
-    detected_language: str
+    translation_lemma: str
+    translation_stressed_lemma: str
+    detected_source_lang: str
+
+
+deduplicated_entries: dict = {}
 
 
 # https://stackoverflow.com/questions/15431044/can-i-set-max-retries-for-requests-request/35504626#35504626
@@ -35,28 +42,48 @@ def call_api(
         response = session.post(url, json=translation_request.dict())
         # Assuming the response is a list of DeeplTranslateResponse objects
         api_result = [DeeplTranslateResponse(**item) for item in response.json()]
+        print( response)
+        print (response.json())
         return api_result
 
 
-def output_csv(batch: List[DeeplTranslateResponse]):
+def deduplicate_and_save_batch(batch: List[DeeplTranslateResponse]):
     for item in batch:
-        print(f'"{item.stressed_text}","{item.translation}"')
+        csv_line = f'"{item.text_stressed_lemma}'
+        if " " in item.text or item.text == item.text_lemma:
+            # phrase or text is the same as lemma. No need to print lemma
+            csv_line += '", '
+        else:
+            csv_line += f' ({item.stressed_text})", '
+        csv_line += f'"{item.translation_stressed_lemma}"'
+
+        if (item.text_stressed_lemma) in deduplicated_entries:
+            saved_line = deduplicated_entries[item.text_stressed_lemma]
+            if len(csv_line) > len(saved_line):
+                # keep the longer item
+                deduplicated_entries[item.text_stressed_lemma] = csv_line
+        else:
+            deduplicated_entries[item.text_stressed_lemma] = csv_line
 
 
-def print_only_if_redirected(message):
+def print_only_if_redirected(message: str):
     if not os.isatty(sys.stdout.fileno()):
         # stdout is not connected to a terminal
         sys.stderr.write(message)
 
 
-def process_file(file_path: str):
+def process_input_file(file_path: str):
     unique_lines = {}
     with open(file_path, "r") as file:
         for line in file:
             line = line.strip()
             if line == "" or line.startswith("#"):
                 continue
+            if not " " in line:
+                line = line[:1].lower() + line[1:]
+            # perform early deduplication
             unique_lines[line] = None
+
     print_only_if_redirected(f"Processing {len(unique_lines)} unique entries.\n")
 
     batch_number = 1
@@ -70,25 +97,28 @@ def process_file(file_path: str):
         for line in batch:
             del unique_lines[line]
         batch_number += 1
-        output_csv(api_result)
+
+        deduplicate_and_save_batch(api_result)
+
+        for line in deduplicated_entries.values():
+            print(line)
 
     print_only_if_redirected("Done.\n")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.stderr.write(f"Usage: {sys.argv[0]} file_to_translate\n")
-        exit(1)
-    process_file(sys.argv[1])
-    exit(0)
+    if len(sys.argv) != 2:
+        sys.stderr.write(f"Usage: {sys.argv[0]} file_to_translate\n\n")
+        sys.stderr.write(
+            f"{sys.argv[0]} creates a csv file suitable for importing into Anki\n"
+        )
+        sys.stderr.write(
+            "   Input is a text file of Ukrainian words or phrases, one to a line.\n"
+            "   Output is written to stdout. If you redirect it to to a file,\n"
+            "    progress will be written to stderr.\n"
+        )
 
-# Bug rendering Ukrainian letters in VSCode Terminal when using Menlo for Powerline: 
-# print("ро́зголосу рете́льно розми́та")
-# If I run the above statement via Run and Debug, the pronunciation stress accent is on the wrong letter in the VSCode terminal output.
-# Same thing happens if I run it via python3 in the VSCode terminal
-# It works correctly if I run the code in Mac iTerm2 or the Mac terminal. 
-# Terminal › External: Osx Exec is set to iterm.app.
-# I use Menlo for Powerline in the VSCode terminal, editor windows, and in iTerm2. There are no issues in the VSCode editors or in iTerm2. 
-# Also, if I copy the incorrectly stressed text from the VSCode terminal into a VSCode editor window or into iTerm2 or terminal, the stress marks appear in the correct places.
-# If I switch the terminal font to Monaco, the stress marks then appear in the correct places.
-# This leads me to believe there is a problem with correctly rendering Ukrainian in the VSCode terminal in Menlo for Powerline
+        exit(1)
+
+    process_input_file(sys.argv[1])
+    exit(0)
